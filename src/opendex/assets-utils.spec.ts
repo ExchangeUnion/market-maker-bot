3c8dd335a84a814c1ad4a45ad6e2de385a7219ae
Karl Ranna <karl@karlranna.com>
2020-06-09 11:30:58 +0000

    move xudBalanceToExchangeAssetAllocation and logAssetBalance spec

---
+++ src/opendex/assets-utils.spec.ts
@@ -0,0 +1,75 @@
+import { BigNumber } from 'bignumber.js';
+import { getLoggers } from '../../test/utils';
+import { GetBalanceResponse } from '../broker/opendex/proto/xudrpc_pb';
+import { Logger } from '../logger';
+import { satsToCoinsStr } from '../utils';
+import { logAssetBalance, xudBalanceToExchangeAssetAllocation } from './assets';
+
+describe('OpenDEX.assets-utils', () => {
+  test('xudBalanceToExchangeAssetAllocation success', () => {
+    const ethBalance = {
+      getChannelBalance: () => 10000000,
+    };
+    const btcBalance = {
+      getChannelBalance: () => 20000000,
+    };
+    const balancesMap = new Map();
+    const baseAsset = 'ETH';
+    const quoteAsset = 'BTC';
+    balancesMap.set(baseAsset, ethBalance);
+    balancesMap.set(quoteAsset, btcBalance);
+    const getBalanceResponse = ({
+      getBalancesMap: () => balancesMap,
+    } as unknown) as GetBalanceResponse;
+    expect(
+      xudBalanceToExchangeAssetAllocation({
+        baseAsset,
+        quoteAsset,
+        balanceResponse: getBalanceResponse,
+      })
+    ).toEqual({
+      baseAssetBalance: new BigNumber(
+        satsToCoinsStr(ethBalance.getChannelBalance())
+      ),
+      quoteAssetBalance: new BigNumber(
+        satsToCoinsStr(btcBalance.getChannelBalance())
+      ),
+    });
+  });
+
+  test('error baseAssetBalance', () => {
+    const balancesMap = new Map();
+    const getBalanceResponse = ({
+      getBalancesMap: () => balancesMap,
+    } as unknown) as GetBalanceResponse;
+    expect(() => {
+      xudBalanceToExchangeAssetAllocation({
+        baseAsset: 'ETH',
+        quoteAsset: 'BTC',
+        balanceResponse: getBalanceResponse,
+      });
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  test('logAssetBalance', () => {
+    const logger = (getLoggers().global as unknown) as Logger;
+    logger.trace = jest.fn();
+    const baseAssetBalance = '1.23456789';
+    const quoteAssetBalance = '9.87654321';
+    const assetBalance = {
+      baseAssetBalance: new BigNumber(baseAssetBalance),
+      quoteAssetBalance: new BigNumber(quoteAssetBalance),
+    };
+    logAssetBalance({
+      logger,
+      assetBalance,
+    });
+    expect(logger.trace).toHaveBeenCalledTimes(1);
+    expect(logger.trace).toHaveBeenCalledWith(
+      expect.stringContaining(baseAssetBalance)
+    );
+    expect(logger.trace).toHaveBeenCalledWith(
+      expect.stringContaining(quoteAssetBalance)
+    );
+  });
+});
--- src/opendex/assets.spec.ts
+++ src/opendex/assets.spec.ts
@@ -1,16 +1,9 @@
-import { TestScheduler } from 'rxjs/testing';
-import {
-  getOpenDEXassets$,
-  xudBalanceToExchangeAssetAllocation,
-  logAssetBalance,
-} from './assets';
 import { Observable } from 'rxjs';
-import { GetBalanceResponse } from '../broker/opendex/proto/xudrpc_pb';
-import { BigNumber } from 'bignumber.js';
+import { TestScheduler } from 'rxjs/testing';
+import { getLoggers, testConfig } from '../../test/utils';
 import { XudClient } from '../broker/opendex/proto/xudrpc_grpc_pb';
-import { testConfig, getLoggers } from '../../test/utils';
-import { Logger } from '../logger';
-import { satsToCoinsStr } from '../utils';
+import { GetBalanceResponse } from '../broker/opendex/proto/xudrpc_pb';
+import { getOpenDEXassets$ } from './assets';
 
 describe('OpenDEX', () => {
   let testScheduler: TestScheduler;
@@ -19,51 +12,6 @@ describe('OpenDEX', () => {
     testScheduler = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
     });
-  });
-
-  test('xudBalanceToExchangeAssetAllocation', () => {
-    const ethBalance = {
-      getChannelBalance: () => 10000000,
-    };
-    const btcBalance = {
-      getChannelBalance: () => 20000000,
-    };
-    const balancesMap = new Map();
-    const baseAsset = 'ETH';
-    const quoteAsset = 'BTC';
-    balancesMap.set(baseAsset, ethBalance);
-    balancesMap.set(quoteAsset, btcBalance);
-    const getBalanceResponse = ({
-      getBalancesMap: () => balancesMap,
-    } as unknown) as GetBalanceResponse;
-    expect(
-      xudBalanceToExchangeAssetAllocation({
-        baseAsset,
-        quoteAsset,
-        balanceResponse: getBalanceResponse,
-      })
-    ).toEqual({
-      baseAssetBalance: new BigNumber(
-        satsToCoinsStr(ethBalance.getChannelBalance())
-      ),
-      quoteAssetBalance: new BigNumber(
-        satsToCoinsStr(btcBalance.getChannelBalance())
-      ),
-    });
-  });
-
-  test('xudBalanceToExchangeAssetAllocation error baseAssetBalance', () => {
-    const balancesMap = new Map();
-    const getBalanceResponse = ({
-      getBalancesMap: () => balancesMap,
-    } as unknown) as GetBalanceResponse;
-    expect(() => {
-      xudBalanceToExchangeAssetAllocation({
-        baseAsset: 'ETH',
-        quoteAsset: 'BTC',
-        balanceResponse: getBalanceResponse,
-      });
-    }).toThrowErrorMatchingSnapshot();
   });
 
   test('getOpenDexAssets$', () => {
@@ -97,27 +45,5 @@ describe('OpenDEX', () => {
       });
       expectObservable(openDEXassets$).toBe(expected);
     });
-  });
-
-  describe('logAssetBalance', () => {
-    const logger = (getLoggers().global as unknown) as Logger;
-    logger.trace = jest.fn();
-    const baseAssetBalance = '1.23456789';
-    const quoteAssetBalance = '9.87654321';
-    const assetBalance = {
-      baseAssetBalance: new BigNumber(baseAssetBalance),
-      quoteAssetBalance: new BigNumber(quoteAssetBalance),
-    };
-    logAssetBalance({
-      logger,
-      assetBalance,
-    });
-    expect(logger.trace).toHaveBeenCalledTimes(1);
-    expect(logger.trace).toHaveBeenCalledWith(
-      expect.stringContaining(baseAssetBalance)
-    );
-    expect(logger.trace).toHaveBeenCalledWith(
-      expect.stringContaining(quoteAssetBalance)
-    );
   });
 });
