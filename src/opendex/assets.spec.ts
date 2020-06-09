49dbb319a42e7cff226c6740be30f1d2e187aa25
Karl Ranna <karl@karlranna.com>
2020-06-09 11:48:33 +0000

    cleanup getOpenDEXassets$ spec

--- src/opendex/assets.spec.ts
+++ src/opendex/assets.spec.ts
@@ -5,45 +5,55 @@ import { XudClient } from '../broker/opendex/proto/xudrpc_grpc_pb';
 import { GetBalanceResponse } from '../broker/opendex/proto/xudrpc_pb';
 import { getOpenDEXassets$ } from './assets';
 
-describe('OpenDEX', () => {
-  let testScheduler: TestScheduler;
+type OpenDEXassetsInputEvents = {
+  xudClient$: string;
+  xudBalance$: string;
+};
 
+let testScheduler: TestScheduler;
+
+const assertOpenDEXassets = (
+  inputEvents: OpenDEXassetsInputEvents,
+  expected: string
+) => {
+  testScheduler.run(helpers => {
+    const { cold, expectObservable } = helpers;
+    const xudBalanceToExchangeAssetAllocation = ({ balanceResponse }: any) =>
+      balanceResponse;
+    const getXudBalance$ = () => {
+      return (cold(inputEvents.xudBalance$) as unknown) as Observable<
+        GetBalanceResponse
+      >;
+    };
+    const getXudClient$ = () => {
+      return (cold(inputEvents.xudClient$) as unknown) as Observable<XudClient>;
+    };
+    const logBalance = () => {};
+    const openDEXassets$ = getOpenDEXassets$({
+      logBalance,
+      config: testConfig(),
+      logger: getLoggers().global,
+      xudClient$: getXudClient$,
+      xudBalance$: getXudBalance$,
+      xudBalanceToExchangeAssetAllocation,
+    });
+    expectObservable(openDEXassets$).toBe(expected);
+  });
+};
+
+describe('getOpenDEXassets$', () => {
   beforeEach(() => {
     testScheduler = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
     });
   });
 
-  test('getOpenDexAssets$', () => {
-    testScheduler.run(helpers => {
-      const { cold, expectObservable } = helpers;
-      const inputEvents = {
-        xudBalance$: '500ms a',
-        xudClient$: '500ms a',
-      };
-      const expected = '1s a';
-      const xudBalanceToExchangeAssetAllocation = ({ balanceResponse }: any) =>
-        balanceResponse;
-      const getXudBalance$ = () => {
-        return (cold(inputEvents.xudBalance$) as unknown) as Observable<
-          GetBalanceResponse
-        >;
-      };
-      const getXudClient$ = () => {
-        return (cold(inputEvents.xudClient$) as unknown) as Observable<
-          XudClient
-        >;
-      };
-      const logBalance = () => {};
-      const openDEXassets$ = getOpenDEXassets$({
-        logBalance,
-        config: testConfig(),
-        logger: getLoggers().global,
-        xudClient$: getXudClient$,
-        xudBalance$: getXudBalance$,
-        xudBalanceToExchangeAssetAllocation,
-      });
-      expectObservable(openDEXassets$).toBe(expected);
-    });
+  test('waits for balance and tradinglimits before emitting', () => {
+    const inputEvents = {
+      xudBalance$: '500ms a',
+      xudClient$: '500ms a',
+    };
+    const expected = '1s a';
+    assertOpenDEXassets(inputEvents, expected);
   });
 });
