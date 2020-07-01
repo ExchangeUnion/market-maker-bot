002628f4f5d84661749472dd948504c6cadbea6f
Karl Ranna <karl@karlranna.com>
2020-07-01 12:54:48 +0000

    ensure CEX$ is only initialized once

--- src/centralized/ccxt/init.spec.ts
+++ src/centralized/ccxt/init.spec.ts
@@ -1,5 +1,5 @@
 import { Dictionary, Exchange, Market } from 'ccxt';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 import { testConfig } from '../../test-utils';
 import { initBinance$ } from './init';
@@ -43,5 +43,32 @@ describe('CCXT', () => {
     };
     const expected = '1s a';
     assertInitBinance(inputEvents, expected);
+  });
+});
+
+it('initializes once and loads markets', done => {
+  expect.assertions(2);
+  const loadMarkets$ = jest.fn(() => {
+    return (of('markets') as unknown) as Observable<Dictionary<Market>>;
+  });
+  const getExchange = jest.fn(() => {
+    return (null as unknown) as Exchange;
+  });
+  const CEX = initBinance$({
+    loadMarkets$,
+    config: testConfig(),
+    getExchange,
+  });
+  // first subscription
+  CEX.subscribe(undefined);
+  setTimeout(() => {
+    CEX.subscribe({
+      complete: () => {
+        // future subscriptions will get cached value
+        expect(getExchange).toHaveBeenCalledTimes(1);
+        expect(loadMarkets$).toHaveBeenCalledTimes(1);
+        done();
+      },
+    });
   });
 });
