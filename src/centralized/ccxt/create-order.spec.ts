c48bd9dac71c14a1460986f964302e88fe26a58e
Karl Ranna <karl@karlranna.com>
2020-07-02 08:07:35 +0000

    get symbol from config for market order

--- src/centralized/ccxt/create-order.spec.ts
+++ src/centralized/ccxt/create-order.spec.ts
@@ -2,20 +2,50 @@ import { createOrder$ } from './create-order';
 import { Exchange } from 'ccxt';
 import { OrderSide } from '../../constants';
 import BigNumber from 'bignumber.js';
+import { testConfig } from '../../test-utils';
+import { merge } from 'rxjs';
 
 describe('CCXT', () => {
-  it('creates market order', done => {
-    expect.assertions(1);
+  it('creates market orders', done => {
+    expect.assertions(5);
     const orderResponse = 'orderResponse';
+    const createMarketOrder = jest.fn(() => Promise.resolve(orderResponse));
     const exchange = ({
-      createMarketOrder: () => Promise.resolve(orderResponse),
+      createMarketOrder,
     } as unknown) as Exchange;
-    const order$ = createOrder$(exchange, OrderSide.BUY, new BigNumber('0.01'));
-    order$.subscribe({
+    const config = testConfig();
+    const orderQuantity = new BigNumber('0.01');
+    const expectedSymbol = `${config.BASEASSET}/${config.QUOTEASSET}`;
+    const buyOrder$ = createOrder$(
+      config,
+      exchange,
+      OrderSide.BUY,
+      orderQuantity
+    );
+    const sellOrder$ = createOrder$(
+      config,
+      exchange,
+      OrderSide.SELL,
+      orderQuantity
+    );
+    merge(buyOrder$, sellOrder$).subscribe({
       next: actualOrderResponse => {
         expect(actualOrderResponse).toEqual(orderResponse);
       },
-      complete: done,
+      complete: () => {
+        expect(createMarketOrder).toHaveBeenCalledTimes(2);
+        expect(createMarketOrder).toHaveBeenCalledWith(
+          expectedSymbol,
+          OrderSide.BUY,
+          orderQuantity.toNumber()
+        );
+        expect(createMarketOrder).toHaveBeenCalledWith(
+          expectedSymbol,
+          OrderSide.SELL,
+          orderQuantity.toNumber()
+        );
+        done();
+      },
     });
   });
 });
--- src/centralized/ccxt/create-order.ts
+++ src/centralized/ccxt/create-order.ts
@@ -2,13 +2,21 @@ import { Exchange, Order } from 'ccxt';
 import { OrderSide } from '../../constants';
 import BigNumber from 'bignumber.js';
 import { Observable, from } from 'rxjs';
+import { Config } from '../../config';
 
 const createOrder$ = (
+  config: Config,
   exchange: Exchange,
   side: OrderSide,
-  amount: BigNumber
+  quantity: BigNumber
 ): Observable<Order> => {
-  return from(exchange.createMarketOrder('ABC', side, amount.toNumber()));
+  return from(
+    exchange.createMarketOrder(
+      `${config.BASEASSET}/${config.QUOTEASSET}`,
+      side,
+      quantity.toNumber()
+    )
+  );
 };
 
 export { createOrder$ };
