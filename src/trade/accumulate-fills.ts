dec1e4a3d69b466c747ce5967de5cb882b5fe22d
Karl Ranna <karl@karlranna.com>
2020-07-21 18:22:43 +0000

    remove accumulateOrderFillsForAssetReceived

--- src/trade/accumulate-fills.ts
+++ src/trade/accumulate-fills.ts
@@ -7,41 +7,6 @@ import { SwapSuccess } from '../proto/xudrpc_pb';
 import { satsToCoinsStr } from '../utils';
 import { curry } from 'ramda';
 
-const accumulateOrderFillsForAssetReceived = (assetReceived: Asset) => {
-  const SEED_VALUE = new BigNumber('0');
-  const ETHaccumulator = (acc: BigNumber, curr: SwapSuccess) => {
-    if (curr.getCurrencyReceived() === assetReceived) {
-      const quantityReceived = new BigNumber(
-        satsToCoinsStr(curr.getAmountReceived())
-      );
-      return acc.plus(quantityReceived);
-    } else {
-      return acc;
-    }
-  };
-  const BTCaccumulator = (acc: BigNumber, curr: SwapSuccess) => {
-    if (curr.getCurrencyReceived() === assetReceived) {
-      const quantitySent = new BigNumber(satsToCoinsStr(curr.getAmountSent()));
-      return acc.plus(quantitySent);
-    } else {
-      return acc;
-    }
-  };
-  const getAccumulator = (assetReceived: Asset) => {
-    switch (assetReceived) {
-      case 'ETH':
-        return ETHaccumulator;
-      case 'BTC':
-        return BTCaccumulator;
-      default:
-        throw new Error('Unrecognized asset to accumulate');
-    }
-  };
-  return (source: Observable<SwapSuccess>) => {
-    return source.pipe(scan(getAccumulator(assetReceived), SEED_VALUE));
-  };
-};
-
 const accumulateOrderFillsForBaseAssetReceived = curry(
   (config: Config, source: Observable<SwapSuccess>) => {
     const SEED_VALUE = new BigNumber('0');
@@ -91,7 +56,6 @@ const accumulateOrderFillsForQuoteAssetReceived = curry(
 );
 
 export {
-  accumulateOrderFillsForAssetReceived,
   accumulateOrderFillsForBaseAssetReceived,
   accumulateOrderFillsForQuoteAssetReceived,
 };
