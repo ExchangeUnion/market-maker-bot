b962cb8cde3e3307437f026a364c6bd6ca73c86a
Karl Ranna <karl@karlranna.com>
2020-07-31 08:41:30 +0000

    feat: restart arby every 6 hours

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, timer, merge } from 'rxjs';
 import { take, tap } from 'rxjs/operators';
 import { curry } from 'ramda';
 
@@ -24,7 +24,13 @@ export const getStartShutdown$ = (): Observable<unknown> => {
   const shutdown$ = new Subject();
   process.on('SIGINT', () => shutdown$.next());
   process.on('SIGTERM', () => shutdown$.next());
-  return shutdown$.asObservable().pipe(take(1));
+  const ONE_MINUTE = 1000 * 60;
+  const ONE_HOUR = ONE_MINUTE * 60;
+  const SIX_HOURS = ONE_HOUR * 6;
+  const restart$ = timer(SIX_HOURS).pipe(
+    tap(() => console.log('Restarting Arby to reduce memory usage.'))
+  );
+  return merge(shutdown$.asObservable(), restart$).pipe(take(1));
 };
 
 const debugObservable = (prefix: string, source: Observable<any>) => {
