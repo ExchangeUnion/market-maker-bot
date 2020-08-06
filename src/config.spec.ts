d1012136162a43fc44bcc1c642b0beb92d921c79
Karl Ranna <karl@karlranna.com>
2020-08-06 08:43:59 +0000

    add trading pair validation to config
    This commit verifies that arby is started with a supported trading pair
    (currently ETH/BTC or BTC/USDT).

---
+++ src/config.spec.ts
@@ -0,0 +1,70 @@
+import { checkConfigOptions } from './config';
+
+describe('checkConfigOptions', () => {
+  it('allows BTC/USDT trading pair', () => {
+    const dotEnvConfig = {
+      LOG_LEVEL: 'trace',
+      BINANCE_API_KEY: '123',
+      BINANCE_API_SECRET: 'abc',
+      DATA_DIR: '/some/data/path',
+      OPENDEX_CERT_PATH: '/some/cert/path',
+      OPENDEX_RPC_HOST: 'localhost',
+      OPENDEX_RPC_PORT: '1234',
+      MARGIN: '0.06',
+      BASEASSET: 'BTC',
+      QUOTEASSET: 'USDT',
+      TEST_CENTRALIZED_EXCHANGE_BASEASSET_BALANCE: '321',
+      TEST_CENTRALIZED_EXCHANGE_QUOTEASSET_BALANCE: '123',
+      LIVE_CEX: 'false',
+    };
+    const verifiedConfig = checkConfigOptions(dotEnvConfig);
+    expect(verifiedConfig).toEqual({
+      ...dotEnvConfig,
+      LIVE_CEX: false,
+    });
+  });
+
+  it('allows ETH/BTC trading pair', () => {
+    const dotEnvConfig = {
+      LOG_LEVEL: 'trace',
+      BINANCE_API_KEY: '123',
+      BINANCE_API_SECRET: 'abc',
+      DATA_DIR: '/some/data/path',
+      OPENDEX_CERT_PATH: '/some/cert/path',
+      OPENDEX_RPC_HOST: 'localhost',
+      OPENDEX_RPC_PORT: '1234',
+      MARGIN: '0.06',
+      BASEASSET: 'ETH',
+      QUOTEASSET: 'BTC',
+      TEST_CENTRALIZED_EXCHANGE_BASEASSET_BALANCE: '321',
+      TEST_CENTRALIZED_EXCHANGE_QUOTEASSET_BALANCE: '123',
+      LIVE_CEX: 'false',
+    };
+    const verifiedConfig = checkConfigOptions(dotEnvConfig);
+    expect(verifiedConfig).toEqual({
+      ...dotEnvConfig,
+      LIVE_CEX: false,
+    });
+  });
+
+  it('does not allow LTC/LTC trading pair', () => {
+    const dotEnvConfig = {
+      LOG_LEVEL: 'trace',
+      BINANCE_API_KEY: '123',
+      BINANCE_API_SECRET: 'abc',
+      DATA_DIR: '/some/data/path',
+      OPENDEX_CERT_PATH: '/some/cert/path',
+      OPENDEX_RPC_HOST: 'localhost',
+      OPENDEX_RPC_PORT: '1234',
+      MARGIN: '0.06',
+      BASEASSET: 'LTC',
+      QUOTEASSET: 'LTC',
+      TEST_CENTRALIZED_EXCHANGE_BASEASSET_BALANCE: '321',
+      TEST_CENTRALIZED_EXCHANGE_QUOTEASSET_BALANCE: '123',
+      LIVE_CEX: 'false',
+    };
+    expect(() => {
+      checkConfigOptions(dotEnvConfig);
+    }).toThrowErrorMatchingSnapshot();
+  });
+});
