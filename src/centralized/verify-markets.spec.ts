d22391ffa155af028415e733c8ad142daa711a57
Karl Ranna <karl@karlranna.com>
2020-10-07 10:48:46 +0000

    fixup! verify CEX assets

--- src/centralized/verify-markets.spec.ts
+++ src/centralized/verify-markets.spec.ts
@@ -4,14 +4,32 @@ import { Dictionary, Market } from 'ccxt';
 
 describe('verifyMarkets', () => {
   const CEXmarkets: Dictionary<Market> = {
-    'BTC/USD': {} as Market,
+    'BTC/USD': {
+      active: true,
+    } as Market,
   };
+
   it('throws when BTC/USD trading pair not exist', () => {
     expect.assertions(1);
     const markets = {
       ...CEXmarkets,
     };
     delete markets['BTC/USD'];
+    const config = {
+      ...testConfig(),
+      ...{ CEX_BASEASSET: 'BTC', CEX_QUOTEASSET: 'USD' },
+    };
+    expect(() => {
+      verifyMarkets(config, markets);
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  it.only('throws when BTC/USD trading pair is inactive', () => {
+    expect.assertions(1);
+    const markets = {
+      ...CEXmarkets,
+    };
+    delete markets['BTC/USD'].active;
     const config = {
       ...testConfig(),
       ...{ CEX_BASEASSET: 'BTC', CEX_QUOTEASSET: 'USD' },
--- src/centralized/verify-markets.ts
+++ src/centralized/verify-markets.ts
@@ -4,7 +4,8 @@ import { errors } from '../opendex/errors';
 
 const verifyMarkets = (config: Config, CEXmarkets: Dictionary<Market>) => {
   const tradingPair = `${config.CEX_BASEASSET}/${config.CEX_QUOTEASSET}`;
-  if (CEXmarkets[tradingPair]) {
+  const market = CEXmarkets[tradingPair];
+  if (market && market.active) {
     return true;
   }
   throw errors.CEX_INVALID_TRADING_PAIR(tradingPair, config.CEX);
