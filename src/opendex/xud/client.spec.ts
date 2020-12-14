c047526d75592ec81587f0a84d9cac3c0c555eb4
scorebreaker <NwcBJn9mFkkAJC@protonmail.com>
2020-12-14 18:20:36 +0200

    fix: cleanup getXudClient$
    XudClient has to be closed when observable completes in order to prevent memory leaks.

--- src/opendex/xud/client.spec.ts
+++ src/opendex/xud/client.spec.ts
@@ -2,23 +2,30 @@ import { XudClient } from '../../proto/xudrpc_grpc_pb';
 import { testConfig } from '../../test-utils';
 import { errors } from '../errors';
 import { getXudClient$ } from './client';
+import { take } from 'rxjs/operators';
 
 jest.mock('../../proto/xudrpc_grpc_pb');
 jest.mock('../../proto/xudrpc_pb');
 
 describe('getXudClient$', () => {
   test('success', done => {
-    expect.assertions(2);
+    expect.assertions(3);
     const config = testConfig();
     const xudClient$ = getXudClient$(config);
-    xudClient$.subscribe(() => {
-      expect(XudClient).toHaveBeenCalledTimes(1);
-      expect(XudClient).toHaveBeenCalledWith(
-        `${config.OPENDEX_RPC_HOST}:${config.OPENDEX_RPC_PORT}`,
-        expect.any(Object),
-        expect.any(Object)
-      );
-      done();
+
+    xudClient$.pipe(take(1)).subscribe({
+      next: client => {
+        expect(XudClient).toHaveBeenCalledTimes(1);
+        expect(XudClient).toHaveBeenCalledWith(
+          `${config.OPENDEX_RPC_HOST}:${config.OPENDEX_RPC_PORT}`,
+          expect.any(Object),
+          expect.any(Object)
+        );
+        setImmediate(() => {
+          expect(client.close).toHaveBeenCalledTimes(1);
+          done();
+        });
+      },
     });
   });
 
